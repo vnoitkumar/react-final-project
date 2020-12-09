aea53193359efdfe69fecf77122b76df6653697d
Vinoth Kumar <vnoitkumar@gmail.com>
2020-12-09 21:01:15 +0530

    Day 1

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+// Login - public (Validation)
+// Home (List) - private - github api
+// Details page - private - dynamic route
+
+// Theme - inside Header
+
+// Maintain the Session of the user
+// Theme details - local storage
+
+export default App;
